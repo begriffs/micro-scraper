04874fafbc7f47e1c7377ea6cf2cdc741cc3cb46
Joe Nelson <cred+github@begriffs.com>
2015-02-11 18:17:42 -0800

    Listen for urls on queue downloads and retrieve them with stats

--- Main.hs
+++ Main.hs
@@ -1,7 +1,16 @@
 import System.Metrics
 import System.Remote.Monitoring.Statsd
-import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
+import Data.String.Conversions (cs)
+import Data.Text hiding (length)
+import Data.Monoid ( (<>) )
+
+import Crypto.Hash.MD5 (hash)
+import Network.HTTP hiding (host)
+import qualified Network.AMQP as AMQP
+
+import qualified System.Metrics.Label as L
+import qualified System.Metrics.Gauge as G
 
 main :: IO ()
 main = do
@@ -9,10 +18,36 @@ main = do
   let opts = defaultStatsdOptions {
       host = "statsd.node.consul"
     , prefix = "scraper"
-    , debug = True
     }
-  registerGcMetrics store
   _ <- forkStatsd opts store
 
-  forever $ do
-    threadDelay 10000000  -- 10s
+  amqpConnection <- AMQP.openConnection "rabbitmq.node.consul" "/" "guest" "guest"
+  amqpChannel <- AMQP.openChannel amqpConnection
+  AMQP.bindQueue amqpChannel "downloads" "" "downloads"
+  forever $
+    AMQP.consumeMsgs amqpChannel "downloads" AMQP.Ack $ download store
+
+download :: Store -> (AMQP.Message, AMQP.Envelope) -> IO ()
+download store (m, env) = do
+  let url = cs $ AMQP.msgBody m
+      md5 = cs $ hash url
+
+  lblUrl    <- createLabel (intercalate "." ["http",md5,"url"])    store
+  lblStatus <- createLabel (intercalate "." ["http",md5,"status"]) store
+  lblLength <- createGauge (intercalate "." ["http",md5,"length"]) store
+
+  L.set lblUrl (cs url)
+
+  resp    <- simpleHTTP $ getRequest (cs url)
+  payload <- getResponseBody resp
+  code    <- getResponseCode resp
+
+  G.set lblLength $ fromIntegral $ length payload
+  L.set lblStatus $ codeText code
+
+  case code of
+       (2,_,_) -> AMQP.ackEnv env
+       _ -> return ()
+
+codeText :: ResponseCode -> Text
+codeText (a,b,c) = cs $ show a <> show b <> show c
